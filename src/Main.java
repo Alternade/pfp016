53c56f0769ed7d721175fbbec6e355d3dff48a5e
BL-Arpit <arpit.sahu@bridgelabz.com>
2023-12-21 09:18:17 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    //public : Access Modifier
+    //static : Access Specifier - Loads into stack memory which can be accessed by multiple object - single copy
+    //void : return type
+    //main : Starting point of execution
+    //String args : A string of arguments taken in by main method
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+
+//git init : used to initialize local repo : done once
+//
