605485272f76451f7a37fba513b334b2efe7a203
BL-Arpit <arpit.sahu@bridgelabz.com>
2023-12-21 09:30:42 +0530

    Added UC1 : To check Employee Attendance

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,21 @@ public class Main {
     //main : Starting point of execution
     //String args : A string of arguments taken in by main method
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int isPresent = 1;
+        int attendance = (int)(Math.random()*10);
+        if(attendance%2 == isPresent){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is absent");
+        }
     }
 }
 
 
 //git init : used to initialize local repo : done once
-//
+//git add : used to stage all the file to save int in the current version : used multiple
+//git commit -m : used to commit or save the build in the local machine
+//git branch -M main : to rename master to main : done once
+//git remote add origin url : it store the url link in the keyword origin : used once
+//git push -u origin main : push the code to remote repository
+//git checkout : command to change branch, -b is used to create a branch,
